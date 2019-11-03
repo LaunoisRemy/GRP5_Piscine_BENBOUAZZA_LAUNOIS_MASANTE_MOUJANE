9b3b0ed021e5142468e957b7df97b53dbb9f3348
Moujane <moujane.ayoub@hotmail.com>
2019-11-03 09:16:58 +0100

    Ajout entité Groupe, Date, methodes tostring

--- site_web/models.py
+++ site_web/models.py
@@ -9,11 +9,18 @@ class Classe(models.Model):
     def __str__(self):
         return self.lib_Classe
 
+class Groupe(models.Model):
+    lib_Groupe=models.CharField(max_length=5)
+
+    def __unicode__(self):
+        return self.lib_Groupe 
+
         
 class Eleve(models.Model):
     nom=models.CharField(max_length=50)
     prenom=models.CharField(max_length=50)
-    classe=models.ForeignKey('Classe',default=None, on_delete=models.CASCADE)
+    classe=models.ForeignKey('Classe',default=None, on_delete=models.CASCADE) #Un élève appartient forcément à une classe
+    groupe=models.ForeignKey('Groupe',default=None, on_delete=models.CASCADE) #Un élève appartient forcément un groupe
 
     def __str__(self):
         return self.nom + " " + self.prenom
@@ -25,3 +32,12 @@ class Question(models.Model):
     id_Question=models.ForeignKey('TOEIC',default=None, on_delete=models.CASCADE) #La réponse à une question correspond à un Toeic + (faut-il représenter la relativité ?)
     #id_Souspartie=models.ForeignKey('sous_Partie',default=None, on_delete=models.CASCADE) #Une réponse est comparée à une réponse d'une sous partie
     reponse_Juste=models.CharField(max_length=1) #À revoir faire quelque chose de plus propre (énumeration ?)
+
+    def __str__(self):
+        return self.id_Question + ":" + self.reponse_Juste
+
+class Date(models.Model):
+    date=models.DateField()
+
+    def __str__(self):
+        return self.date
