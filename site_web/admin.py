94a1472afe8ec2eedc82b5596919cd95157a62b1
Launois Remy <remy.launois@gmail.com>
2019-11-04 18:36:06 +0100

    Changement dans la base de donnee
    Ajout sous partie et Resultats
    
    Modification de certains models

--- site_web/admin.py
+++ site_web/admin.py
@@ -7,9 +7,13 @@ class EleveAdmin(admin.ModelAdmin):
     class Meta:
         model=Eleve
     fieldsets=[
-        ("Eleve",{"fields" : ["nom","prenom"]}),
-        ("Classe",{"fields" : ["classe"]})
+        ("Eleve",{"fields" : ["nom","prenom","num_INE"]}),
+        ("Appartient",{"fields" : ["classe","groupe"]})
 
     ]
 admin.site.register(Eleve,EleveAdmin)
 admin.site.register(Classe)
+admin.site.register(Groupe)
+admin.site.register(Question)
+admin.site.register(TOEIC)
+admin.site.register(Sous_partie)
--- site_web/models.py
+++ site_web/models.py
@@ -12,31 +12,51 @@ class Classe(models.Model):
 class Groupe(models.Model):
     lib_Groupe=models.CharField(max_length=5)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.lib_Groupe 
 
         
 class Eleve(models.Model):
+    num_INE=models.CharField(max_length=11,primary_key=True )
     nom=models.CharField(max_length=50)
     prenom=models.CharField(max_length=50)
     classe=models.ForeignKey('Classe',default=None, on_delete=models.CASCADE) #Un élève appartient forcément à une classe
-    groupe=models.ForeignKey('Groupe',default=None, on_delete=models.CASCADE) #Un élève appartient forcément un groupe
+    groupe=models.ForeignKey('Groupe',default=None, on_delete=models.CASCADE) #Un élève appartient forcément un groupe peut-etre pas en cascade
 
     def __str__(self):
         return self.nom + " " + self.prenom
 
 class TOEIC(models.Model):
-    LibelleTOEIC=models.CharField(max_length=20)
+    lib_TOEIC=models.CharField(max_length=20)
+    def __str__(self):
+        return self.lib_TOEIC
 
 class Question(models.Model):
-    id_Question=models.ForeignKey('TOEIC',default=None, on_delete=models.CASCADE) #La réponse à une question correspond à un Toeic + (faut-il représenter la relativité ?)
-    #id_Souspartie=models.ForeignKey('sous_Partie',default=None, on_delete=models.CASCADE) #Une réponse est comparée à une réponse d'une sous partie
+    id_Question=models.CharField(max_length=1 )
+    id_TOEIC=models.ForeignKey('TOEIC',default=None, on_delete=models.CASCADE) #La réponse à une question correspond à un Toeic 
+    id_SousPartie=models.ForeignKey('Sous_partie',default=None, on_delete=models.CASCADE) 
     reponse_Juste=models.CharField(max_length=1) #À revoir faire quelque chose de plus propre (énumeration ?)
 
+    class Meta:
+        unique_together = (("id_Question","id_TOEIC"),)
     def __str__(self):
-        return self.id_Question + ":" + self.reponse_Juste
+        return str(self.id_TOEIC) + ", question " + self.id_Question + " : " + self.reponse_Juste
 
-class Date(models.Model):
+class Sous_partie(models.Model): 
+    lib_Partie=models.CharField(max_length=1) 
+    type_Partie=models.CharField(max_length=15) 
+    def __str__(self):
+        return "Partie numéro : "+ self.lib_Partie +" " + self.type_Partie
+
+class Reponse(models.Model): 
+    num_Ine=models.ForeignKey('Eleve',default=None, on_delete=models.CASCADE)  
+    id_TOEIC=models.ForeignKey('TOEIC',default=None, on_delete=models.CASCADE)  
+    id_SousPartie=models.ForeignKey('Sous_partie',default=None, on_delete=models.CASCADE) 
+    score=models.CharField(max_length=2) 
+    def __str__(self):
+        return "TOEIC : "+self.id_TOEIC +"Partie : " + self.id_SousPartie + " score de :" + self.scoreqX
+
+class Date(models.Model):#surment inutile
     date=models.DateField()
 
     def __str__(self):
