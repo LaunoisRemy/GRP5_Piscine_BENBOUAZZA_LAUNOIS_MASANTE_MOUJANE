e6c1f153e6d86b308b1aecd37da6f3f328baa479
Anna <anana@wanadoo.fr>
2019-12-07 17:21:11 +0100

    Ajout de truc en ligne

--- site_web/forms.py
+++ site_web/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-
 class qcm(forms.Form):
     CHOICES = (('a','a'),
                ('b','b'),
                ('c','c'),
                ('d','d'),)
-    picked = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple())
+    picked = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
+
+
