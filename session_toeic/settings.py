01d17964d1de14f143b443e3bc74f5aaa60c7a37
Moujane <moujane.ayoub@hotmail.com>
2020-01-11 12:28:42 +0100

    Ajout du calcu de la moyenne par partie, et du calcul de la médiane, ajout de la liste qui permet de retrouver les notes, manque a les représenter sous forme de graphique, problème graphique polaire se est représenté en ligne au lieu d'être en polaire

--- session_toeic/settings.py
+++ session_toeic/settings.py
@@ -43,9 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'site_web', J'ai du l'enlever pour que chartit marche
-    'chartit',
-    'site_web.apps.SiteWebConfig',
+    'site_web.apps.SiteWebConfig'
 ]
 
 MIDDLEWARE = [
--- site_web/views.py
+++ site_web/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.models import User
 
 from chartit import DataPool, Chart
 
+import statistics
 import json
 # Create your views here.
 
@@ -155,54 +156,93 @@ def filtre_note_par_partie(request):
 
     score1 = [0,0,0,0,0,0,0]
     cat1 = ["0","1","2","3","4","5","6"]
+    moy1=0
+    effectiftot=0
+    print(requete1)
     for j in requete1:
-        score1[j['score']]+=j['the_count']
+        score1[j['score']]+=j['the_count'] #Pour un score on a un effectif de personne qui ont eu cette note
+        moy1+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+        effectiftot+=j['the_count']    # sur l'effectif total
+
+    moy1=moy1/effectiftot
+    mediane1=0
+    i=0
+    k=0
+    print("score1",score1)
+    while (mediane1<effectiftot/2) :
+        mediane1+=score1[k]
+        k+=1
+    print(k)
+    print('mediane1',k)
+
+    
+
+    print("MMMMOOOOOOOOO",moy1/effectiftot)
+    
     score1 = json.dumps(score1)
     cat1 = json.dumps(cat1)
     
     score2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     cat2 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"]
+    moy2=0
     for j in requete2:
         score2[j['score']]+=j['the_count']
+        moy2+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+    moy2=moy2/effectiftot
     
     score2 = json.dumps(score2)
     cat2 = json.dumps(cat2)
 
     score3=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     cat3 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39"]
+    moy3=0
     for j in requete3:
         score3[j['score']]+=j['the_count']
+        moy3+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+    moy3=moy3/effectiftot
     
     score3 = json.dumps(score3)
     cat3 = json.dumps(cat3)
 
     score4=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     cat4 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"]
+    moy4=0
     for j in requete4:
         score4[j['score']]+=j['the_count']
+        moy4+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+    moy4=moy4/effectiftot
     
     score4 = json.dumps(score4)
     cat4 = json.dumps(cat4)
 
     score5=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     cat5 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"]
+    moy5=0
     for j in requete5:
         score5[j['score']]+=j['the_count']
+        moy5+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+    moy5=moy5/effectiftot
     
     score5 = json.dumps(score5)
     cat5 = json.dumps(cat5)
 
     score6 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     cat6 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
+    moy6=0
     for j in requete6:
         score6[j['score']]+=j['the_count']
+        moy6+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+    moy6=moy6/effectiftot
     score6 = json.dumps(score6)
     cat6 = json.dumps(cat6)
     
     score7=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     cat7 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54"]
+    moy7 = 0
     for j in requete7:
         score7[j['score']]+=j['the_count']
+        moy7+=j['score']*j['the_count'] # La moyenne est la somme des points total 
+    moy7=moy7/effectiftot
     
     score7 = json.dumps(score7)
     cat7 = json.dumps(cat7)
@@ -223,6 +263,15 @@ def filtre_note_par_partie(request):
     scoreL = json.dumps(scoreL)
     catL = json.dumps(catL)
 
+    a=[0,1,2,3,4,5,6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+    for i in range(len(a)):
+        a[i]=NOTE_L(a[i])
+    print(a)
+    R=[5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 65, 70, 80, 85, 90, 95, 100, 110, 115, 120, 125, 130, 140, 145, 150, 160, 165, 170, 175, 180, 190, 195, 200, 210, 215, 220, 225, 230, 235, 240, 250, 255, 260, 265, 270, 280, 285, 290, 300, 305, 310, 320, 325, 330, 335, 340, 350, 355, 360, 365, 370, 380, 385, 390, 395, 400, 405, 410, 415, 420, 425, 430, 435, 445, 450, 455, 465, 470, 480, 485, 490, 495, 495, 495, 495]
+    L=[5, 5, 5, 5, 5, 5, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 110, 115, 120, 125, 130, 135, 140, 145, 150, 160, 165, 170, 175, 180, 185, 190, 195, 200, 210, 215, 220, 230, 240, 245, 250, 255, 260, 270, 275, 280, 290, 295, 300, 310, 315, 320, 325, 330, 340, 345, 350, 360, 365, 370, 380, 385, 390, 395, 400, 405, 410, 420, 425, 430, 440, 445, 450, 460, 465, 470, 475, 480, 485, 490, 495, 495, 495, 495, 495, 495, 495, 495, 495, 495, 495]
+
+    parties=["Partie 1","Partie 2","Partie 3","Partie 4","Partie 5","Partie 6","Partie7"]
+    moyennes=[moy1,moy2,moy3,moy4,moy5,moy6,moy7]
     return render(request,'espace_prof/search_user.html',{'cat1':cat1,'score1':score1,'cat2':cat2,'score2':score2,'cat3':cat3,'score3':score3,'cat4':cat4,'score4':score4,'cat5':cat5,'score5':score5,'cat6':cat6,'score6':score6,'cat7':cat7,'score7':score7,'catR':catR,'scoreR':scoreR,'catL':catL,'scoreL':scoreL,'filter': user_filter})
 
 
