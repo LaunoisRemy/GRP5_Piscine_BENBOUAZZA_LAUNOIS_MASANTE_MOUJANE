c393befb9eb418ce175dcc9fba839bd26244c6c5
Moujane <moujane.ayoub@hotmail.com>
2020-01-14 09:02:28 +0100

    Modification du filtre et de la forme du bouton et du placement des graphes

--- site_web/filters.py
+++ site_web/filters.py
@@ -13,7 +13,8 @@ class FiltreNoteParPartie(django_filters.FilterSet):
 
     class Meta:
         model = ScoreParPartie
-        fields = ['id_TOEICEnCours__id_TOEIC', 'id_Eleve__classe__lib_Classe','id_Eleve__groupe__lib_Groupe','id_Eleve__classe__annee_Promo','id_Eleve__nom','id_Eleve__prenom'
+        id_Eleve = django_filters.ModelChoiceFilter(field_name='Oui')
+        fields = ['id_TOEICEnCours__id_TOEIC', 'id_Eleve','id_Eleve__groupe','id_Eleve__classe'
          ]
 
          # On filtre par toeic, classe, groupe de td, année de promo
