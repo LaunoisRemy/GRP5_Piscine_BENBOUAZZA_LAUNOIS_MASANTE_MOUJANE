df6a3a264183c4bbeaf8795d10c1b7f19b1b231c
Anna <spatchoun@gmail.com>
2020-01-15 10:53:57 +0100

    Changement url

--- session_toeic/settings.py
+++ session_toeic/settings.py
@@ -130,12 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')#'tcspprtpscn'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')#'AaAr.admin1234'
+EMAIL_HOST_USER = 'tcspprtpscn'
+EMAIL_HOST_PASSWORD = 'AaAr.admin1234'
